19575b37854f037e53cff6aab3077d8c27d67bb5
docandrew <jonfandrew@gmail.com>
2016-05-31 18:41:39 +0000

    Add Brainf*ck Interpreter written in D

---
+++ bfInterpret.d
@@ -0,0 +1,191 @@
+#!/usr/local/bin/rdmd
+
+import std.stdio;
+import core.thread;
+
+string code = 
+`
+[ This program prints "Hello World!" and a newline to the screen, its
+  length is 106 active command characters. [It is not the shortest.]
+
+  This loop is a "comment loop", a simple way of adding a comment
+  to a BF program such that you don't have to worry about any command
+  characters. Any ".", ",", "+", "-", "<" and ">" characters are simply
+  ignored, the "[" and "]" characters just have to be balanced. This
+  loop and the commands it contains are ignored because the current cell
+  defaults to a value of 0; the 0 value causes this loop to be skipped.
+]
+++++++++               Set Cell #0 to 8
+[
+    >++++               Add 4 to Cell #1; this will always set Cell #1 to 4
+    [                   as the cell will be cleared by the loop
+        >++             Add 2 to Cell #2
+        >+++            Add 3 to Cell #3
+        >+++            Add 3 to Cell #4
+        >+              Add 1 to Cell #5
+        <<<<-           Decrement the loop counter in Cell #1
+    ]                   Loop till Cell #1 is zero; number of iterations is 4
+    >+                  Add 1 to Cell #2
+    >+                  Add 1 to Cell #3
+    >-                  Subtract 1 from Cell #4
+    >>+                 Add 1 to Cell #6
+    [<]                 Move back to the first zero cell you find; this will
+                        be Cell #1 which was cleared by the previous loop
+    <-                  Decrement the loop Counter in Cell #0
+]                       Loop till Cell #0 is zero; number of iterations is 8
+
+The result of this is:
+Cell No :   0   1   2   3   4   5   6
+Contents:   0   0  72 104  88  32   8
+Pointer :   ^
+
+>>.                     Cell #2 has value 72 which is 'H'
+>---.                   Subtract 3 from Cell #3 to get 101 which is 'e'
++++++++..+++.           Likewise for 'llo' from Cell #3
+>>.                     Cell #5 is 32 for the space
+<-.                     Subtract 1 from Cell #4 for 87 to give a 'W'
+<.                      Cell #3 was set to 'o' from the end of 'Hello'
++++.------.--------.    Cell #3 for 'rl' and 'd'
+>>+.                    Add 1 to Cell #5 gives us an exclamation point
+>++.                    And finally a newline from Cell #6
+`;
+
+void main()
+{
+	writeln("Jon's Brainf**k Interpreter v0.1");
+	runBF(code);
+}
+
+void runBF(string bfCode, bool debugging = false)
+{
+	byte[30_000] bfarr;
+	uint dataPointer = 0;
+	uint programPointer = 0;
+	uint scopeLevel = 0;		//keep track of [ and ]s
+	uint lastBookmark = 0;
+
+	do
+	{
+		char a = bfCode[programPointer];
+
+		if(debugging)
+		{
+			writefln("program pointer: %d, dataPointer: %d, code: %c, data: %d", programPointer, dataPointer, a, bfarr[dataPointer]);
+			Thread.sleep(dur!("msecs")(50));
+		}
+
+		switch(a)
+		{
+			case '>':
+			{
+				dataPointer++;
+				programPointer++;
+				break;
+			}
+			case '<':
+			{
+				dataPointer--;
+				programPointer++;
+				break;
+			}
+			case '+':
+			{
+				bfarr[dataPointer]++;
+				programPointer++;
+				break;
+			}
+			case '-':
+			{
+				bfarr[dataPointer]--;
+				programPointer++;
+				break;
+			}
+			case '.':
+			{
+				write(cast(char)bfarr[dataPointer]);
+				programPointer++;
+				break;
+			}
+			case ',':
+			{
+				bfarr[dataPointer] = readln()[0];	//just take first character from read string
+				programPointer++;
+				break;
+			}
+			case '[':
+			{
+				uint ignoreNextNBrackets = 0;
+
+				if(bfarr[dataPointer] == 0)
+				{
+					do
+					{
+						programPointer++;
+
+						if(bfCode[programPointer] == '[')
+						{
+							ignoreNextNBrackets++;
+						}
+						else if(bfCode[programPointer] == ']' && ignoreNextNBrackets > 0)
+						{
+							ignoreNextNBrackets--;
+						}
+						else if(bfCode[programPointer] == ']' && ignoreNextNBrackets == 0)
+						{
+							break;
+						}
+
+					}while(programPointer < bfCode.length);
+				}
+
+				programPointer++;
+				break;
+			}
+			case ']':
+			{
+				uint ignorePreviousNBrackets = false;
+
+				if(bfarr[dataPointer] != 0)
+				{
+					//now search backwards to find the [ at this ]'s scope level
+					//ignore every [ that has a ] after it.
+					do
+					{
+						programPointer--;
+						
+						if(bfCode[programPointer] == ']')
+						{
+							//we're inside a previous scope as we rewind the loop, so the next [ we encounter
+							// can be safely ignored
+							ignorePreviousNBrackets++;
+						}
+						else if(bfCode[programPointer] == '[' && ignorePreviousNBrackets > 0)
+						{
+							//we're ignoring this [, but the next one might be ours
+							ignorePreviousNBrackets--;
+						}
+						else if(bfCode[programPointer] == '[' && ignorePreviousNBrackets == 0)
+						{
+							break;
+						}
+
+					}while(programPointer > 0);
+
+					programPointer++;
+				}
+				else
+				{
+					programPointer++;
+				}
+
+				break;
+			}
+			default:
+			{
+				programPointer++;
+				break;
+			}
+		}
+
+	}while(programPointer < bfCode.length);
+}
